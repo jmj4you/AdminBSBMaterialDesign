92a94d4b3288fff50de62087f7541c5689b70095
JOSEPH M J <joseph@aleksundshantu.com>
2018-09-28 14:29:59 +0530

    ajax data table

--- js/pages/tables/jquery-datatable.js
+++ js/pages/tables/jquery-datatable.js
@@ -1,3 +1,11 @@
+/**
+ *  For more examples check website
+ *
+ *  SERVER SIDE/AJAX => https://www.datatables.net/examples/server_side/simple.html
+ *
+ *  Search links => https://www.google.com/search?q=hhow+to+manage+thousands+of+records+in+datatable&ie=utf-8&oe=utf-8&client=firefox-b-ab
+ */
+
 $(function () {
     $('.js-basic-example').DataTable({
         responsive: true
@@ -10,5 +18,11 @@ $(function () {
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
         ]
+    });
+
+    $('#ajax-example').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "ajax.json"
     });
 });
